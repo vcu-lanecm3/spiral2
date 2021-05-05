c7fce3a61af2331fe470951edaacda2ec21909a2
vcu-Lanecm3 <lanecm3@vcu.edu>
2021-05-05 14:12:30 -0400

    update

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,8 +1,6 @@
 def spiralize(size, n=1):
-
     if size != 1:
         return_value = (4 * size * size) - (6 * size) + 6 + spiralize(size - 2, n)
-
     else:
         return_value = n  # returning n for the last function iteration to add, finally, the middle number
 
