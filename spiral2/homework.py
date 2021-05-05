f29e3387f9c81879c6fe8e3eaf4b101d7aacd048
vcu-Lanecm3 <lanecm3@vcu.edu>
2021-05-05 14:01:52 -0400

    updated Code

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,8 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
+    if size != 1:
+        return_value = (4 * size * size) - (6 * size) + 6 + spiralize(size - 2, n)
+
+    else:
+        return_value = n + 1
+
     return return_value
